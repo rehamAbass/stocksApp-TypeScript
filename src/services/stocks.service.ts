96dcf7054ed5d81c22446307a1087cb211fb8368
Reham Abass <soft.rehamabass@gmail.com>
2022-05-15 15:29:03 +0300

    ..

--- src/services/stocks.service.ts
+++ src/services/stocks.service.ts
@@ -9,11 +9,14 @@ export class StocksService {
   //paramters: 'FB,AAPL'
   getStocks(parameters: string) {
     const headers = new HttpHeaders()
+      .set("Access-Control-Content-Type","*")
       .set('Content-Type', 'application/json')
       .set('Access-Control-Allow-Origin', '*')
       .set('Access-Control-Allow-Credentials', 'true')
       .set('X-RapidAPI-Host', 'yh-finance.p.rapidapi.com')
-      .set('X-RapidAPI-Key', '78902fb637msh224990667c48a20p178cd0jsnf5b4c97c76b9');
+      .set('X-RapidAPI-Key', '78902fb637msh224990667c48a20p178cd0jsnf5b4c97c76b9')
+      .set("Access-Control-Accept", "*");
+    
     
     let params = new HttpParams();
     params = params.append('symbols', parameters);
